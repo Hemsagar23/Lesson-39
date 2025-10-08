05a5857003274bf7ace0b162b5c427b7554955be
Hemsagar23 <kolluhemsagar@gmail.com>
2025-10-08 20:17:48 +0530

    initial commit

---
+++ index.py
@@ -0,0 +1,8 @@
+num = int(input("Enter The Number You want to check: "))
+print("The number needed to be checked is: ", num)
+
+if num%2==0:
+    print("Number is Even")
+    
+else:
+    print("Number is Odd")
